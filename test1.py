5a6b2f942305b90987589a7929aae37267d02010
Potato-OvO <1224141267@qq.com>
2022-02-25 16:21:51 +0800

    文档更新

--- test1.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
-
-url = "https://www.baidu.com/"
-
-driver.get("https://kns.cnki.net/kcms/detail/detail.aspx?dbcode=CAPJ&dbname=CAPJLAST&filename=HDJJ20220222005")
